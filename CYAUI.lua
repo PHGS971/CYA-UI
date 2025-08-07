66cd13476f9da0736e167fc6c2b0ff1325183282
PHGS_dev <phtechnologes@gmail.com>
2025-08-07 19:21:33 -0300

    Update CYAUI.lua

--- CYAUI.lua
+++ CYAUI.lua
@@ -55,3 +55,24 @@ function CYAUI:CreateWindow(config)
 end
 
 return CYAUI
+
+local CYA = loadstring(game:HttpGet("https://raw.githubusercontent.com/PHGS971/CYA-UI/main/CYAUI.lua"))()
+
+CYA:PlayIntro(function()
+    local Window = CYA:CreateWindow({
+        Title = "Meu Hub",
+        Icon = "rbxassetid://10723423456",
+        Author = "MeuNome",
+        Size = UDim2.fromOffset(580, 300),
+        Theme = "Dark"
+    })
+
+    local Tab1 = Window:CreateTab("Principal")
+    local Tab2 = Window:CreateTab("Configurações")
+
+    -- Exemplo de conteúdo
+    local Elements = loadstring(game:HttpGet("https://raw.githubusercontent.com/PHGS971/CYA-UI/main/Library/Elements.lua"))()
+    Elements:CreateButton(Tab1, "Executar Script", function()
+        print("Executando...")
+    end)
+end)
