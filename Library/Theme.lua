a1a5876fda6dd75dab39d8489de547b55c07d254
PHGS_dev <phtechnologes@gmail.com>
2025-08-07 19:16:55 -0300

    Create Theme.lua

---
+++ Library/Theme.lua
@@ -0,0 +1,13 @@
+local Theme = {}
+
+Theme.Dark = {
+    Background = Color3.fromRGB(25, 25, 25),
+    Text = Color3.fromRGB(255, 255, 255)
+}
+
+Theme.Light = {
+    Background = Color3.fromRGB(240, 240, 240),
+    Text = Color3.fromRGB(0, 0, 0)
+}
+
+return Theme
